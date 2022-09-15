e21edf0e5e13baf75b2f75795d29509307c5e516
Jack Stoops <jstoops@unc.edu>
2022-09-14 23:35:45 -0400

    I FINISHED

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,15 +2,17 @@
 
 __author__ = "730489241"
 
-"""Define input_word as user's input """
-def input_guess(word_len: int) -> str:
 
-    user_guess: str = input(f"Enter a {word_len} characrter word: ")
+def input_guess(word_len: int) -> str:
+    """Define input_word as user's input."""
+    user_guess: str = input(f"Enter a {word_len} character word: ")
     while len(user_guess) != word_len:
         user_guess = input(f"That wasn't {word_len} chars! Try again: ")   
     return user_guess
 
+
 def contains_char(secret: str, input_char: str) -> bool:
+    """Compares Characters in secret and input_char."""
     assert len(input_char) == 1
     i = 0
     while i < len(secret):
@@ -20,7 +22,9 @@ def contains_char(secret: str, input_char: str) -> bool:
             i += 1
     return False
 
+
 def emojified(input_word: str, secret: str) -> str:
+    """Adds the boxes."""
     assert len(input_word) == len(secret)    
     green_box: str = "\U0001F7E9"
     yellow_box: str = "\U0001F7E8"
@@ -37,14 +41,15 @@ def emojified(input_word: str, secret: str) -> str:
         i += 1
     return final
 
+
 def main() -> None:
+    """Executes the main function."""
     turn: int = 1
     secret: str = "codes"
     secret_len: int = len(secret)
     yellow_box: str = "\U0001F7E8"
     white_box: str = "\U00002B1C"
-    i = 0
-
+    has_lost: bool = True
     while turn <= 6:
         print(f"=== Turn {turn}/6 ===")
         input_word: str = input_guess(secret_len)
@@ -53,10 +58,12 @@ def main() -> None:
         
         if not (contains_char(boxes, yellow_box) or contains_char(boxes, white_box)):
             print(f"You won in {turn}/6 turns!")
-            exit()
+            turn = 7
+            has_lost = False
         else:
-          turn += 1
-    print("X/6 - Sorry, try again tomorrow!")
+            turn += 1
+    if has_lost:
+        print("X/6 - Sorry, try again tomorrow!")
 
 
 if __name__ == "__main__":
