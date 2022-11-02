ffcaab9ef260a40b764bf7618ac8c7a652bc63ff
Jack Stoops <jstoops@unc.edu>
2022-11-01 23:48:04 -0400

    i did something

---
+++ exercises/ex08/data_utilities.py
@@ -0,0 +1,86 @@
+"""EX 08 - Utility Functions."""
+
+__author__ = "730489241"
+
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of csv into a table."""
+    result: list[dict[str, str]] = []
+    
+    # Open a handle to the data file
+    file_handle = open(filename, "r", encoding="utf8")
+
+    # Prepare to read the data file as a CSV rather thn just strings
+    csv_reader = DictReader(file_handle)
+    
+    # Read each row of the CSV line-by-line
+    for row in csv_reader:
+        result.append(row)
+
+    # Close the file when we're done, to free its resources.
+    file_handle.close()
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row: 
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(given: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Produce a new column-based table, given only the first N rows of data for each column."""
+    result: dict[str, list[str]] = {}
+    for column in given:
+        if N >= len(given[column]):
+            return given
+        else: 
+            first_n_values: list[str] = []
+            i: int = 0
+            while i < N:
+                value: str = given[column][i]
+                first_n_values.append(value)
+                i += 1
+            result[column] = first_n_values
+    return result
+
+
+def select(given: dict[str, list[str]], column_names: list[str]) -> dict[str, list[str]]:
+    """Produce new column-based table given only a specific subset of the original columns."""
+    result: dict[str, list[str]] = {}
+    for column in column_names:
+        result[column] = given[column]
+    return result
+
+
+def concat(first: dict[str, list[str]], second: dict[str, list[str]]) -> dict[str, list[str]]:
+    """Produce a new column-based table with 2 column-based combined."""
+    result: dict[str, list[str]] = {}
+    # copy key-value pairs from first dictionary to result
+    for strings in first: 
+        result[strings] = first[strings]
+        # copy key-value pairs from second dictionary to result
+    for strings in second:
+        if strings in result: 
+            for items in second[strings]:
+                result[strings].append(items)
+        else:
+            result[strings] = second[strings]
+    return result
--- lessons/classes_objects.py
+++ lessons/classes_objects.py
@@ -1,19 +1,22 @@
-"""ECample of a class and object instantiation."""
+"""Example of a class and object instantiation."""
 
 
 class Pizza:
     """Models the idea of a Pizza."""
 
     # Attribute Definitions
-    size: str
-    toppings: int
-    extra_cheese: bool
+    size: str = "small"
+    toppings: int = 0
+    extra_cheese: bool = False
 
+    def __init__(self, size: str, toppings: int):
+        """Constructor definition for initialization of attributes."""
+        self.size = size
+        self.toppings = toppings
 
-    def price(self) -> float:
+    def price(self, tax: float) -> float:
         """Calculate the price of a Pizza"""
         total: float = 0.0
-        # TODO -- compute price
         if self.size == "large":
             total += 10.0
         else:
@@ -24,14 +27,21 @@ class Pizza:
         if self.extra_cheese:
             total += 1.0
 
+        total *= tax
+
         return total
 
 
-a_pizza: Pizza = Pizza()
+a_pizza: Pizza = Pizza("large", 3)
 a_pizza.size = "large"
 a_pizza.toppings = 3
 a_pizza.extra_cheese = False
 print(Pizza)
 print(a_pizza)
 print(a_pizza.size)
-print(f"Price: ${a_pizza.price()}")
+print(f"Price: ${a_pizza.price(1.05)}")
+
+another_pizza: Pizza = Pizza("small", 0)
+another_pizza.extra_cheese = True
+print(another_pizza.size)
+print(f"Price: ${another_pizza.price(1.05)}")
